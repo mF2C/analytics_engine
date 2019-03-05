09fd7f6b6e0e5f00c60c1b9bb26905a4a78a8014
sridhar voorakkara <sridhar.voorakkara@intel.com>
2019-03-05 14:15:03 +0000

    Adding endpoint for node subgraph telemetry

--- test_client/rest_API_client_analyse.py
+++ test_client/rest_API_client_analyse.py
@@ -13,8 +13,8 @@ headers = {'Content-type': 'application/json', 'Accept': 'text/json'}
 # data=json.dumps(payload)
 service_json = {'service_id': 'clearwater_ims',
                 "name": "clearwater_ims",
-                "ts_from":'1550756320',
-                "ts_to":'1550756330',
+                "ts_from":'1551088710',
+                "ts_to":'1551088720',
                 "description": "test workload",
                 "resourceURI": '/cpu_stress',
                 "exec": 'helloworld',
@@ -35,6 +35,10 @@ service_json = {'service_id': 'clearwater_ims',
                             "air_pressure": False,
                             "ir_motion": False}
                         }
+
+# service_json = {'category': {'battery level': False, 'jammer': False, 'ir_motion': False, 'air_pressure': False, 'door sensor': False, 'pump sensor': False, 'temperature': False, 'disk': 'low', 'network': 'low', 'accelerometer': False, 'humidity': False, 'location': False, 'memory': 'low', 'inclinometer': False, 'cpu': 'low'},
+#                 'ts_from': '1550755977', 'name': 'clearwater_ims', 'resourceURI': '/clearwater_ims', 'exec_type': 'docker', 'service_id': 'clearwater_ims', 'exec': 'helloworld', 'ts_to': '1550756040', 'description': 'clearwater_ims'}
+
 
 url = 'http://localhost:46020/mf2c/analyse'
 res = requests.post(url, json=service_json, headers=headers)
